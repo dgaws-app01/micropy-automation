58c03bcf633236244ee23941d249c01f553b5429
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2020-08-09 17:22:53 +0530

    Update interface.py

--- esp8266/interface.py
+++ esp8266/interface.py
@@ -1,4 +1,9 @@
+import time
 import network
 
+strt_tm = time.ticks_ms()
 
-print('Loaded : %s'%(__name__))
+
+
+end_tm = time.ticks_ms()
+print('%d : Loaded "%s" | MSec Taken : %d '%(strt_tm, __name__, end_tm - strt_tm))
