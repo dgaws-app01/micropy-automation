3112b14f82e5c342681f3cb2fe20c4c70ad0dbb0
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2020-08-11 16:59:28 +0530

    Update interface.py

--- esp8266/interface.py
+++ esp8266/interface.py
@@ -1,9 +1,18 @@
 import time
 import network
+#import config file
 
 strt_tm = time.ticks_ms()
 
-
+# if config.type = "ROOT"
+  # connect to WIFI , SSID = config.parentname
+# if config.type = "BRANCH"
+  # connect to WIFI, SSID = config.parentname + config.parentIndex
+  # also setup as AP, AP-SSID = config.name + config.index
+# if config.type = "LEAF"
+  # connect to WIFI, SSID = config.parentname + config.parentIndex
+  
+#print all details related to network - interface Status, Connectivity Stat , IP, MAC, Signal
 
 end_tm = time.ticks_ms()
 print('%d : Loaded "%s" | MSec Taken : %d '%(strt_tm, __name__, end_tm - strt_tm))
