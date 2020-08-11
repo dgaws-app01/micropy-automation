fc152209310abdb14cd5d02a450db294ae6cf951
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2020-08-11 17:22:02 +0530

    Update peripherals.py

--- esp8266/peripherals.py
+++ esp8266/peripherals.py
@@ -1,1 +1,17 @@
+import machine;
 
+class BoardPins:
+  def __init__(self):
+    pass
+  
+  def on(int pin, int delayMS = 0):
+    pass
+
+  def off(int pin, int delayMS = 0):
+    pass
+
+  def isOn():
+    pass
+
+  def volt(int pin, int volt, int durationMS = 0, int delayMS = 0):
+    pass
