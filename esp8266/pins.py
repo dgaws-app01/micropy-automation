13071df43f276259d94abbcbaafa4201ca3a2f5d
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2020-08-13 01:37:16 -0700

    Update pins.py

--- esp8266/pins.py
+++ esp8266/pins.py
@@ -1,9 +1,9 @@
-
+# Write your code here :-)
 import machine
 import time
 
 class BoardPinOut:
-    def __init__(self, gpio, name, mode, busy = True, aka = ""):
+    def __init__(self, gpio, name, mode, busy = True, aka = "", pinId="L15"):
         self.gpio = gpio
         self.name = name
         self.mode = mode
@@ -32,13 +32,13 @@ class BoardPinOut:
             self.pin.off()
     
     
-D0 = BoardPinOut(16, "D0", machine.Pin.OUT, True, "Wake,redLED")
-D1 = BoardPinOut(5, "D1", machine.Pin.OUT, True, "SCL")
-D2 = BoardPinOut(4, "D2", machine.Pin.OUT, True, "SDA")
-D3 = BoardPinOut(0, "D3", machine.Pin.OUT, True, "FLASH")
-D4 = BoardPinOut(2, "D4", machine.Pin.OUT, True, "TXD1")
+D0 = BoardPinOut(16, "D0", machine.Pin.OUT, True, "Wake,RedLED", "R15")
+D1 = BoardPinOut(5, "D1", machine.Pin.OUT, True, "SCL", "R14")
+D2 = BoardPinOut(4, "D2", machine.Pin.OUT, True, "SDA", "R13")
+D3 = BoardPinOut(0, "D3", machine.Pin.OUT, True, "FLASH", "R12")
+D4 = BoardPinOut(2, "D4", machine.Pin.OUT, True, "TXD1,BlueLED", "R11")
 
-D = D3
+D = D4
 #d0 = {gpio:16, mode:machine.Pin.OUT, busy:True, pin:machine.Pin( gpiopin , machine.Pin.OUT), aka: "wake,redLED"}
 
 D.on()
