de20670ae3d085ba1d9a8c213a72c668439c8774
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2020-08-13 01:25:48 -0700

    Update pins.py

--- esp8266/pins.py
+++ esp8266/pins.py
@@ -2,12 +2,45 @@
 import machine
 import time
 
-d0 = {gpio:16, mode:machine.Pin.OUT, busy:True, pin:machine.Pin( gpiopin , machine.Pin.OUT), aka: "wake,redLED"}
+class BoardPinOut:
+    def __init__(self, gpio, name, mode, busy = True, aka = "":
+        self.gpio = gpio
+        self.name = name
+        self.mode = mode
+        self.busy = busy
+        self.aka = aka
+        self.pin = machine.Pin(gpio, mode)
+        if busy:
+            self.engage()
+    
+    def engage(self):
+        self.busy = True
+        
+    def release(self):
+        self.busy = False
+        
+    def changeMode(self,mode):
+        self.mode = mode
+        self.pin = machine.Pin(self.gpio, self.mode)
+        
+    def On(self):
+        if self.mode == machine.Pin.OUT:
+            self.pin.on()
+    
+    def Off(self):
+        if self.mode == machine.Pin.OUT:
+            self.pin.off()
+    
+    
+d0 = BoardPinOut(16, "d0", machine.Pin.OUT, True, "wake,redLED")
 
-d0.pin.on()
+
+#d0 = {gpio:16, mode:machine.Pin.OUT, busy:True, pin:machine.Pin( gpiopin , machine.Pin.OUT), aka: "wake,redLED"}
+
+d0.on()
 time.sleep(1)
-d0.pin.off()
+d0.off()
 time.sleep(1)
-d0.pin.on()
+d0.on()
 time.sleep(1)
-d0.pin.off()
+d0.off()
